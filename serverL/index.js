cfc8c538691c1ca3cb245a8c3b1b0449337d1f26
Aryan Biswas <aryanb198@gmail.com>
2024-06-04 23:30:39 +0530

    change

--- serverL/index.js
+++ serverL/index.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin: "https://invoice-ninja-aryan.vercel.app",
+        origin: "*",
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
     })
